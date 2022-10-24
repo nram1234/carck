284e1b209575c84999d817c49ff1d72cfc8786e9
redanaser0@gmail.com <Nram1598562>
2022-10-24 17:04:48 +0200

    l

--- lib/scr/notf/notification_scr.dart
+++ lib/scr/notf/notification_scr.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../utility/app_colors.dart';
 import 'notification_controller.dart';
 
 class NotificationSCR extends GetView<NotificationController> {
@@ -8,66 +9,72 @@ class NotificationSCR extends GetView<NotificationController> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Scaffold(
-        appBar: AppBar(centerTitle: true, title: Text("تنبيهات"), actions: []),
-        body: RefreshIndicator(onRefresh: controller.onRefresh ,
-          child: GetBuilder<NotificationController>(builder: (logic) {
+    return SafeArea(
 
-            return
-              logic.loading
-                ? Center(
-                    child: CircularProgressIndicator(),
-                  )
-                :
-            ListView.builder(controller: logic.scrollController,
-                    itemCount: logic.allNotifications?.length,
-                    itemBuilder: (context, pos) {
-                      return Directionality(
-                          textDirection: TextDirection.rtl,
-                          child: Row(
-                            children: [
-                              // Container(
-                              //   height: 50,
-                              //   width: 50,
-                              //   decoration: BoxDecoration(
-                              //       image: DecorationImage(
-                              //           image: AssetImage("assets/aler.png"))),
-                              // ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Expanded(
-                                  child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+      child: Scaffold(
+     appBar: AppBar(centerTitle: true, title: Text("تنبيهات"), actions: [],backgroundColor:ColorApp. primaryColor),
+          body: RefreshIndicator(onRefresh: controller.onRefresh ,
+            child: GetBuilder<NotificationController>(builder: (logic) {
+
+              return
+                logic.loading
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  :
+              ListView.builder(controller: logic.scrollController,
+                      itemCount: logic.allNotifications?.length,
+                      itemBuilder: (context, pos) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Directionality(
+                              textDirection: TextDirection.rtl,
+                              child: Row(
                                 children: [
-                                  Text(logic .allNotifications![pos].title!),
+                                  // Container(
+                                  //   height: 50,
+                                  //   width: 50,
+                                  //   decoration: BoxDecoration(
+                                  //       image: DecorationImage(
+                                  //           image: AssetImage("assets/aler.png"))),
+                                  // ),
                                   SizedBox(
-                                    height: 2,
+                                    width: 10,
                                   ),
-                                  Row(
+                                  Expanded(
+                                      child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
                                     children: [
-                                      // Container(
-                                      //   height: 20,
-                                      //   width: 20,
-                                      //   decoration: BoxDecoration(
-                                      //       image: DecorationImage(
-                                      //     image: AssetImage(
-                                      //       "assets/clock.png",
-                                      //     ),
-                                      //   )),
-                                      // ),
+                                      Text(logic .allNotifications![pos].title!),
                                       SizedBox(
-                                        width: 8,
+                                        height: 2,
                                       ),
-                                      Text(logic .allNotifications![pos].body!)
+                                      Row(
+                                        children: [
+                                          // Container(
+                                          //   height: 20,
+                                          //   width: 20,
+                                          //   decoration: BoxDecoration(
+                                          //       image: DecorationImage(
+                                          //     image: AssetImage(
+                                          //       "assets/clock.png",
+                                          //     ),
+                                          //   )),
+                                          // ),
+                                          SizedBox(
+                                            width: 8,
+                                          ),
+                                          Text(logic .allNotifications![pos].body!)
+                                        ],
+                                      ),
                                     ],
-                                  ),
+                                  ))
                                 ],
-                              ))
-                            ],
-                          ));
-                    });
-          }),
-        ));
+                              )),
+                        );
+                      });
+            }),
+          )),
+    );
   }
 }
