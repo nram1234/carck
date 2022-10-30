03983468ed273a29b98e9b1a3efe4a0b4bc1c2ef
redanaser0@gmail.com <Nram1598562>
2022-10-30 10:40:07 +0200

    l

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
    // loctionServ.llocationService();
-    return GetMaterialApp(
+    return GetMaterialApp(debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
